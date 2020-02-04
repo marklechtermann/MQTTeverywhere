53d1c549467af5ac84b7dc2480b5abd78da55242
Mark Lechtermann <mark.lechtermann@gmail.com>
2020-02-04 22:11:34 +0100

    Add Java MQTT Client

---
+++ java/src/main/java/mqttjava/Callback.java
@@ -0,0 +1,25 @@
+package mqttjava;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class Callback implements MqttCallback {
+
+    @Override
+    public void connectionLost(Throwable cause) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println(String.format("Received: %s :%s", topic, message));
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+    }
+
+}
